8d1d26a53eb3d367fa438ff94bc310e377d7361a
armansajedi944 <armansajedi944@gmail.com>
2024-06-17 14:20:15 -0700

    match expression and else_if

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+fn main() {
+
+
+    //activity 3
+
+let message : bool = true;
+
+if message == true {
+
+    println!("hello");
+    
+}else if  message == false {
+    println!("goodbye");
+    
+}
+
+
+
+   // activity 4
+
+
+   let num = 19;
+
+   if num> 5{
+    println!(" number is higher than 5");
+   }else if num < 5{
+    println!(" number is lower than 5");
+   }else if num==5{
+    println!("number is equal to 5");
+   }
+
+   // activity 5 match expression in rust
+
+   let some_int = 4;
+   match some_int {
+    1=> println!("its 1"),
+    2=> println!("its 2 "),
+    3=> println!("its 3"),
+    _=> println!("its something else"),
+       
+   }
+
+   let decition = true;
+
+   match decition {
+
+    true => println!("its ture"),
+    false => println!("its a false "),
+    _ => println!(" its about text"),
+       
+   }
+
+   // activity match expression b
+
+   
+
+
+
+
+}
